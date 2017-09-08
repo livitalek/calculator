3cbbbc6aa8927714d5ed180b673cd37f4f9bf56b
Alexander Liwicki <aleksandr_livitski@mail.ru>
2017-09-08 21:15:20 +0700

    Change code in files function.c and function.h in directory src

---
+++ src/function.h
@@ -0,0 +1,10 @@
+#ifndef H_CALCULATOR
+#define H_CALCULATOR
+
+float myplus(float numb1, float numb2);
+float myminus(float numb1, float numb2);
+float mymultiplication(float numb1, float numb2);
+float mydivision(float numb1, float numb2);
+float mypower(float numb1, int numb2);
+
+#endif
