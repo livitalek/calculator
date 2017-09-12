e9c25b6634ce05a7dbab233326d40a11436cd97d
Alexander Liwicki <aleksandr_livitski@mail.ru>
2017-09-12 19:55:11 +0700

    Add for calclutor help in program

--- src/main.c
+++ src/main.c
@@ -10,6 +10,7 @@ int main(int argc, char **argv) {
 	char chMenu;
 	bool flagMenu = true;
 	while (flagMenu) {
+		system("clear");
 		printf("1 - Begin\n2 - Help\n3 - Exit\n");
 		chMenu = getchar();
 		switch (chMenu) {
@@ -17,9 +18,11 @@ int main(int argc, char **argv) {
 			calculatorBegin();
 			break;
 		case '2':
+			calculatorHelp();
 			break;
 		case '3':
 		case 27:
+			system("clear");
 			flagMenu = false;
 			break;
 		default:
@@ -34,6 +37,7 @@ void calculatorBegin() {
 	char chIntoNumb;
 	char chCalcBegin;
 	bool flagCB = true;
+	system("clear");
 	while (flagCB) {
 		scanf("%f%c%f", &numb1, &chIntoNumb, &numb2 );
 		system("clear");
@@ -55,10 +59,35 @@ void calculatorBegin() {
 			break;
 		default:
 			printf("ERROE\n");
+			break;
 		}
 		chCalcBegin = getchar();
 		if (chCalcBegin == 27) {
+			system("clean");
 			flagCB = false;
 		}
 	}
 }
+
+void calculatorHelp() {
+	char chCalcHelp;
+	bool flagCalcHelp = true;
+	while (flagCalcHelp) {
+		system("clear");
+		printf("Help in program 'Calculator'\n");
+		printf("Data input: <number 1> character <number 2>\n");
+		printf("Characters:\n");
+		printf("\t+ - scaling\n");
+		printf("\t- - subtraction\n");
+		printf("\t* - multiplication\n");
+		printf("\t/ - division\n");
+		printf("\t^ - degree\n");
+		chCalcHelp = getchar();
+		if (chCalcHelp == 27) {
+			flagCalcHelp = false;
+		}
+	
+	}
+}
+
+  
